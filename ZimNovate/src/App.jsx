9ccd3283347772303b3930a6531c45404491402b
vincentmugondora <vinmugondora@gmail.com>
2026-01-30 14:18:09 +0200

    added pages

--- ZimNovate/src/App.jsx
+++ ZimNovate/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import SiteLayout from './components/SiteLayout.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Services from './pages/Services.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Contact from './pages/Contact.jsx'
+import NotFound from './pages/NotFound.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route element={<SiteLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
